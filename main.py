4e01436f7b1046d56a22738f07945551098161bd
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-30 21:53:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,55 @@ Rafeek=Client(
 @Rafeek.on_message(filters.command("start"))
 async def start_message(bot, message):
     await message.reply_text(
-        text="👋 𝙷𝙴𝚈 𝙷𝙾𝚆 𝙰𝚁𝙴 𝚈𝙾𝚄?",
+        text="👋 𝙷𝙴𝙻𝙾 {}\n\n⎆ 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>\n\n🕵️ 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂,\n\n➕ 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍\n\n👮‍♂ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href='https://t.me/Rafeeq_Kunnimon'>★ 𝚁𝚊𝚏𝚎𝚚 ★",
         reply_markup=InlineKeyboardMarkup( [[
-             InlineKeyboardButton("😎 𝙹𝙾𝙸𝙽 𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 😎", url="https://t.me/MovieRosterOfficial")
-            ]]
+             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ➕', url='http://t.me/Movie_Roster_bot?startgroup=true')
+            ],[
+            InlineKeyboardButton('✨ ᴡᴏʀᴋɪɴɢ ɢʀᴏᴜᴘ ✨', url='https://t.me/MovieRosterGroup')
+            ],[
+            InlineKeyboardButton('🕵‍♂ ᴅᴇᴠᴇʟᴏᴘᴇʀ 🕵‍♂', url='https://t.me/Rafeeq_Kunnimon'),
+            InlineKeyboardButton('❤️ sᴜᴘᴘᴏʀᴛ ❤️', url='https://t.me/MovieRosterOfficial')
+            ],[
+            InlineKeyboardButton('📚 ʜᴇʟᴘ 📚', callback_data='help'),
+            InlineKeyboardButton('🔰 ᴀʙᴏᴜᴛ 🔰', callback_data='about')
+        ]]
             )
        )
 
 @Rafeek.on_message(filters.command("help"))
 async def start_message(bot, message):
     await message.reply_text(
-        text="🤠 𝚙𝚘𝚍𝚎 𝚖𝚎𝚢𝚛𝚎 🤠",
+        text="👋 ʜᴇʟʟᴏᴡ {}\nʜᴇʀᴇ ɪᴅ ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴍʏ\nᴄᴏᴍᴍᴀɴᴅᴀ.",
         reply_markup=InlineKeyboardMarkup( [[
-             InlineKeyboardButton("😎 𝙹𝙾𝙸𝙽 𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 😎", url="https://t.me/MovieRosterOfficial"),
-             InlineKeyboardButton("😋 𝚊 𝚠𝚘𝚎𝚛 😋", url="https://t.me/MovieRosterOfficial")
-            ]]
+             InlineKeyboardButton('👮‍♂ ᴀᴅᴍɪɴ 👮‍♂', callback_data='admin'),
+            InlineKeyboardButton('🔌 ᴄᴏɴɴᴇᴄᴛ 🔌', callback_data='coct'),
+            ],[
+            InlineKeyboardButton('🖥️ ғɪʟᴛᴇʀs 🖥️', callback_data='auto_manual'),      
+            InlineKeyboardButton('🖨️ ɢᴛʀᴀɴs 🖨️', callback_data='gtrans'),
+            ],[
+            InlineKeyboardButton('ℹ️ ɪɴғᴏ ℹ️', callback_data='info'),
+            InlineKeyboardButton('🧾 ᴍᴇᴍᴇs 🧾', callback_data='memes'),
+            ],[
+            InlineKeyboardButton('🔖 ᴘᴀsᴛᴇ 🔖', callback_data='paste'),
+            InlineKeyboardButton('📟 ᴘᴀssᴡᴏʀᴅ ɢᴇɴ 📟', callback_data='genpassword'),
+            ],[
+            InlineKeyboardButton('📌 ᴘɪɴ 📌', callback_data='pin'),
+            InlineKeyboardButton('🎧 ᴘᴜʀɢᴇ 🎧', callback_data='purge'),
+            ],[
+            InlineKeyboardButton('🎯 ʀᴇsᴛʀɪᴄ 🎯', callback_data='restric'),
+            InlineKeyboardButton('🔎 sᴇᴀʀᴄʜ 🔍', callback_data='search'),
+            ],[
+            InlineKeyboardButton('⭕ sʜᴀʀᴇ ᴛᴇxᴛ ⭕', callback_data='sharetext'),
+            InlineKeyboardButton('🎶 ᴍᴜsɪᴄ 🎶', callback_data='music'),
+            ],[
+            InlineKeyboardButton('🎵 ᴛᴛ-sᴘᴇᴇᴄʜ 🎵', callback_data='tts'),
+            InlineKeyboardButton('📋 ᴛɢʀᴀᴘʜ 📋', callback_data='tgraph'),
+            ],[
+            InlineKeyboardButton('🔸 ᴛᴇxᴛ sʜᴏʀᴛɴᴇʀ 🔹', callback_data='shortner'),
+            InlineKeyboardButton('🧟 ᴢᴏᴍʙɪᴇs 🧟', callback_data='zombies'),
+            ],[
+            InlineKeyboardButton('✖️ ʙᴀᴄᴋ ✖️', callback_data='start')
+        ]]
             )
        )
 
