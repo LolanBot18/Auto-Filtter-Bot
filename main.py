0c7e3068768fb3e27524efcde369d0dc50a6c60d
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-30 16:03:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+Muhammad=Client(
+    "Auto Filter Bot",
+    bot_token="5128376307:AAEnMFvurvU2lpdhHsvJC0G-pJRf_Rdt5bs",
+    api_id="6327652",
+    api_hash="6c2ee4bf150d9f3cd3e3d64aad0772c8"
+)
+
+@Muhammad.on_message(filters.command("start))
+asnyc def start_message(bot, message):
+    await message.reply_text("👋 Hey How are You")
+
+@Muhammad.on_message(filters.command("start))
+asnyc def help(bot: Muhammad message: Message):
+    await message.reply_text("❌ Not Help You Ok 😐")
+
+Muhammad.run()
