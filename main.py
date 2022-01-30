4a8d9818bb8ad062873a7e3c67e8dcde1e924330
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-30 20:36:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-Muhammad=Client(
+Rafeek=Client(
     "Auto Filter Bot",
     bot_token="5128376307:AAEnMFvurvU2lpdhHsvJC0G-pJRf_Rdt5bs",
     api_id="6327652",
     api_hash="6c2ee4bf150d9f3cd3e3d64aad0772c8"
 )
 
-@Muhammad.on_message(filters.command("start"))
+@Rafeek.on_message(filters.command("start"))
 async def start_message(bot, message):
-    await message.reply_text("👋 Hey How are You")
+    await message.reply_text(
+        text="👋 𝙷𝙴𝚈 𝙷𝙾𝚆 𝙰𝚁𝙴 𝚈𝙾𝚄?",
+        reply_markup=InlineKeyboardMarkup
+            InlineKeyboardButton("😎 𝙹𝙾𝙸𝙽 𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 😎", url="https://t.me/MovieRosterOfficial")
+            ]]
+            )
+        )
 
-@Muhammad.on_message(filters.command("help"))
-async def help(bot: Muhammad, message: Message):
-    await message.reply_text("❌ Not Help You Ok 😐")
+@Rafeek.on_message(filters.command("help"))
+async def start_message(bot, message):
+    await message.reply_text(
+        text="🤠 𝚙𝚘𝚍𝚎 𝚖𝚎𝚢𝚛𝚎 🤠",
+        reply_markup=InlineKeyboardMarkup
+            InlineKeyboardButton("😎 𝙹𝙾𝙸𝙽 𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 😎", url="https://t.me/MovieRosterOfficial"),
+            InlineKeyboardButton ("😋 𝚊 𝚠𝚘𝚎𝚛 😋", url=https://t.me/MovieRosterOfficial")
+            ]]
+            )
+        )
 
-Muhammad.run()
+Rafeek.run()
