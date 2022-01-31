d878e43696ed778d7c6b806183e026576df7bb4a
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-31 21:12:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 Rafeek=Client(
     "Auto Filter Bot",
-    bot_token="5128376307:AAEnMFvurvU2lpdhHsvJC0G-pJRf_Rdt5bs",
-    api_id="6327652",
-    api_hash="6c2ee4bf150d9f3cd3e3d64aad0772c8"
+    bot_token="bot token",
+    api_id="api id",
+    api_hash="api hash"
 )
 
 @Rafeek.on_message(filters.command("start"))
